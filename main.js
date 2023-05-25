f3ae198efa72f9b1f99c31752f1d886cf7c33e16
Rupal Jain <31166523+RupalJain97@users.noreply.github.com>
2023-05-25 11:02:53 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,49 @@
+// var x, y, width, height, margin, xaxis, yaxis;
+// let data;
+
+d3.select("#show_a01").on("click", function () {
+    d3.select("#assign1").attr("hidden", null);
+});
+d3.select("#hide_a01").on("click", function () {
+    d3.select("#assign1").attr("hidden", true);
+});
+
+
+d3.select("#show_a02").on("click", function () {
+    d3.select("#assign2").attr("hidden", null);
+});
+d3.select("#hide_a02").on("click", function () {
+    d3.select("#assign2").attr("hidden", true);
+});
+
+
+d3.select("#show_a03").on("click", function () {
+    d3.select("#assign3").attr("hidden", null);
+});
+d3.select("#hide_a03").on("click", function () {
+    d3.select("#assign3").attr("hidden", true);
+});
+
+
+d3.select("#show_a04").on("click", function () {
+    d3.select("#assign4").attr("hidden", null);
+});
+d3.select("#hide_a04").on("click", function () {
+    d3.select("#assign4").attr("hidden", true);
+});
+
+
+d3.select("#show_a05").on("click", function () {
+    d3.select("#assign5").attr("hidden", null);
+});
+d3.select("#hide_a05").on("click", function () {
+    d3.select("#assign5").attr("hidden", true);
+});
+
+
+d3.select("#show_a06").on("click", function () {
+    d3.select("#assign6").attr("hidden", null);
+});
+d3.select("#hide_a06").on("click", function () {
+    d3.select("#assign6").attr("hidden", true);
+});
